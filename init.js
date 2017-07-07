74d163687776f36342bf9bbf02cc4f1117993fd6
elzup <guild0105@gmail.com>
2017-07-08 03:20:55 +0900

    Illumination

--- init.js
+++ init.js
@@ -3,39 +3,43 @@ load('api_gpio.js');
 load('api_http.js');
 load('api_sys.js');
 load('api_timer.js');
+load('api_neopixel.js');
 
-// Helper C function get_led_gpio_pin() in src/main.c returns built-in LED GPIO
-let led = 4;
-let isLain = false;
+let pin = 5, numPixels = 16, colorOrder = NeoPixel.RGB;
+let strip = NeoPixel.create(pin, numPixels, colorOrder);
 
-let getInfo = function() {
-  return JSON.stringify({total_ram: Sys.total_ram(), free_ram: Sys.free_ram()});
-};
-
-GPIO.write(led, GPIO.INT_LEVEL_LO);
-
+let gen = 0;
+let isLain = true;
 
 // Blink built-in LED every second
-GPIO.set_mode(led, GPIO.MODE_OUTPUT);
-Timer.set(1000 * 5 * 60 /* 1 sec */, true /* repeat */, function() {
+Timer.set(1000 * 60 * 5 /* 5 min */, true /* repeat */, function() {
   HTTP.query({
     url: 'https://map.yahooapis.jp/weather/V1/place?coordinates=139.8066112,35.7482911&output=json&appid=dj00aiZpPWxFTFlCMWJETjlMTyZzPWNvbnN1bWVyc2VjcmV0Jng9NjY-',
     success: function(body, full_http_msg) {
       // print(body);
       let b = JSON.parse(body);
       let v = b.Feature[0].Property.WeatherList.Weather[1].Rainfall;
-      // v = 4;
+      v = 4;
       isLain = v >= 1;
       if (isLain) {
-        GPIO.write(led, GPIO.INT_LEVEL_LO);
+        strip.clear();
+        strip.show();
       }
     },
     error: function(err) { print(err); }  // Optional
   });
 }, null);
 
-Timer.set(500, true, function() {
+Timer.set(300, true, function() {
+  strip.clear();
+  gen = (gen + 1) % 16;
   if (isLain) {
-    GPIO.toggle(led);
+    for (let i = 0; i < 16; i++) {
+      let level = (i + gen) % 8;
+      strip.setPixel(i, 0, 0, level * 10);
+      print(i);
+      print(level * 10);
+    }
+    strip.show();
   }
 }, null);
