5fe2a2eae7bf080a158b110a96519079bc32781a
elzup <guild0105@gmail.com>
2017-07-08 12:52:05 +0900

    RCT State api

--- init.js
+++ init.js
@@ -1,46 +1,48 @@
 load('api_config.js');
+load('api_rpc.js');
 load('api_http.js');
 load('api_sys.js');
 load('api_timer.js');
 load('api_neopixel.js');
 
-let pin = 5, numPixels = 16, colorOrder = NeoPixel.RGB;
+let pin = 4, numPixels = 16, colorOrder = NeoPixel.RGB;
 let strip = NeoPixel.create(pin, numPixels, colorOrder);
 strip.clear();
 strip.show();
 
-let gen = 0;
-let isLain = false;
 
-// Blink built-in LED every second
-Timer.set(1000 * 60 * 5 /* 5 min */, true /* repeat */, function() {
-  HTTP.query({
-    url: 'https://map.yahooapis.jp/weather/V1/place?coordinates=139.8066112,35.7482911&output=json&appid=dj00aiZpPWxFTFlCMWJETjlMTyZzPWNvbnN1bWVyc2VjcmV0Jng9NjY-',
-    success: function(body, full_http_msg) {
-      // print(body);
-      let b = JSON.parse(body);
-      let v = b.Feature[0].Property.WeatherList.Weather[1].Rainfall;
-      isLain = v >= 1;
-      if (isLain) {
-        strip.clear();
-        strip.show();
-      }
-    },
-    error: function(err) { print(err); }  // Optional
-  });
-}, null);
+let state = {};
+for (let i = 0; i < 16; ++i) {
+    state[i] = { "on": true, "color": { "r": 255, "g": 255, "b": 255 } };
+}
 
-Timer.set(300, true, function() {
-  strip.clear();
-  // isLain = true;
-  if (isLain) {
-    gen = (gen + 1) % 8;
-    for (let i = 0; i < 16; i++) {
-      let level = ((i < 8 ? i : 8 - i % 8) + gen) % 8;
-      strip.setPixel(i, 0, 0, level * 10);
-      // print(i);
-      // print(level * 10);
+RPC.addHandler('Control', function(args) {
+    let start = args.start;
+    let end = args.end;
+    let color = args.color;
+    for (let i = start; i <= end; ++i) {
+        state[i].color = color;
     }
+    strip.setPixel(i, 0, 0, level * 10);
+});
+
+let gen = true;
+
+Timer.set(1000, true, function() {
+    strip.clear();
+    gen = !gen;
+    if (gen) {
+        for (let i = 0; i < 16; i++) {
+            let s = state[i];
+            if (!s.on) {
+                continue;
+            }
+            strip.setPixel(i, s.color.g, s.color.r, s.color.b);
+            // print(i);
+            // print(level * 10);
+        }
+    }
+    print(state[i].color.on);
+    print(state[i].color.g);
     strip.show();
-  }
 }, null);
